994a3bf0ab38a827caba2ab234af44a7e63efea2
imbeltagy <imbeltagy@gmail.com>
2026-02-24 10:51:56 +0200

    style(home): add title to categories section

--- src/sections/home/category-groups-list.tsx
+++ src/sections/home/category-groups-list.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useTranslations } from "next-intl";
+
 import { Box, Grid, Stack, Container, Typography } from "@mui/material";
 
 import { SECTION_PADDING } from "@/layouts/config-layout";
@@ -13,15 +15,38 @@ interface Props {
 }
 
 export default function CategoryGroupsList({ groups }: Props) {
+  const t = useTranslations("Pages.Home");
+  const hasGroups = groups.some((group) => group.categories.length > 0);
+
   return (
     <Container
       sx={{
         width: "100%",
         position: "relative",
         pb: SECTION_PADDING,
+        pt: 4,
       }}
     >
       <Stack spacing={4}>
+        {hasGroups && (
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Box
+              sx={{
+                width: 6,
+                my: 0.5,
+                alignSelf: "stretch",
+                borderRadius: "50px",
+                backgroundColor: "primary.light",
+              }}
+            />
+            <Typography component="span" fontSize={24}>
+              🛍️
+            </Typography>
+            <Typography variant="h4" component="h2">
+              {t("categories_by_group_title")}
+            </Typography>
+          </Stack>
+        )}
         {groups
           .filter((group) => group.categories.length > 0)
           .map(({ id, name, categories }) => (
